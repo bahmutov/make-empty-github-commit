ca8abfc261928701420f81508296d9a168e770ea
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-02 10:18:08 -0400

    init repo

---
+++ src/make-empty-github-commit-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const makeEmptyGithubCommit = require('.')
+
+describe('make-empty-github-commit', () => {
+  it('write this test', () => {
+    console.assert(makeEmptyGithubCommit, 'should export something')
+  })
+})
